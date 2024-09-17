f3acd5cc283257c025167e3cee012a4a0dc0f9fd
Lucas Dillon <ldillon2@uwyo.edu>
2024-09-17 00:22:56 +0000

    Finished Lab 2

--- README.md
+++ README.md
@@ -1,12 +1,7 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
-## Worked with/sources 
-* example
-* example
-* example
-* example
+# [Lucas Dillon]
+## [Lab 2]
+## [Submission date: 09/16/24]
+## Worked with/sources:
+* NA
 ## Project Quirks/ Things that don't work
-* example
-* example
-* example
+* NA
--- cosc1010-lab02.py
+++ cosc1010-lab02.py
@@ -1,41 +1,49 @@
-# Your Name Here
+# Lucas Dillon
 # UWYO COSC 1010
-# Submission Date
+# 9/16/2024
 # Lab 02 
-# Lab Section: 
-# Sources, people worked with, help given to: 
-# your
-# comments
-# here
+# Lab Section: 16
+# Sources, people worked with, help given to: NA
+
+# I checked on canvas for my lab section, I believe it is section 16 as
+# that is the only section held at 6:10 to 8:00 on Mondays.
 
 your_variable_here = "when you see this, replace it with your code"
 
 ## Section ONE
 
 # Complete the following print statement to print out "Hello, COSC1010"
-print()
+print("Hello, COSC1010")
 
 # Assign the string above to a variable named hello_message and print that variable
-hello_message = ""
-print("Your code here")
+hello_message = "Hello, COSC1010"
+print(hello_message)
 
 # Assign the string "cowboy joe" to a variable, and print that variable with title casing
-print("Your code here")
+titlevar = "cowboy joe"
+print(titlevar.title())
 
 # Complete the following f-string print message 
     # You will need to create your own variables and insert them  
     # the final message should read `The University of Wyoming was founded in 1886`
 
-print(f"The {your_variable_here} was founded in {your_variable_here}")
+uwyo = "University of Wyoming"
+date = "1886"
+
+print(f"The {uwyo} was founded in {date}")
 
 # Now let's do some math with variables 
     # Create two variables x and y and assign them the values 5 and 10 respectively 
     # Complete the following print statements using your variables
     #All math must be done within the braces in the f-strings
-print(f"x + y = {your_variable_here}")
-print(f"x - y = {your_variable_here}")
-print(f"x * y = {your_variable_here}")
-print(f"x / y = {your_variable_here}")
+
+x = 5
+y = 10
+
+print(f"x + y = {x+y}")
+print(f"x - y = {x-y}")
+print(f"x * y = {x*y}")
+print(f"x / y = {x/y}")
 
 # String concatenation 
     # Finally we will take a look at string concatenation
@@ -46,3 +54,15 @@ print(f"x / y = {your_variable_here}")
         # last_name, which is your last name
         # space, which is a space character 
     # Use string concatenation to print out your full name 
+
+first_name = "Lucas"
+last_name = "Dillon"
+space = " "
+
+full_name = first_name + space + last_name
+print(full_name)
+
+#Concatenation does not work between types, for example you cannot
+#concatenate integers and strings.
+# print(1 + ", two") would give "unsupported operand types"
+# print(1 + 3) is not concatenation, but integer addition.
